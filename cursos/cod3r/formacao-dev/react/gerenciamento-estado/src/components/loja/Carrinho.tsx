b32084c1ce3fccaf80c37e4db0c522b95c8f2550
Gabriel Zemuner <gabrielzemuner@live.com>
2026-02-02 21:55:56 -0300

    finalizacao projeto gerenciamento estado loja

--- cursos/cod3r/formacao-dev/react/gerenciamento-estado/src/components/loja/Carrinho.tsx
+++ cursos/cod3r/formacao-dev/react/gerenciamento-estado/src/components/loja/Carrinho.tsx
@@ -2,12 +2,34 @@ import { useContext } from "react";
 import Area from "../template/Area";
 import CarrinhoVazio from "./CarrinhoVazio";
 import CarrinhoContext from "@/context/CarrinhoContext";
+import ItemCarrinhoCard from "./ItemCarrinhoCard";
+import Real from "@/utils/Real";
+import { IconX } from "@tabler/icons-react";
 
 export default function Carrinho() {
-  const { itens } = useContext(CarrinhoContext);
+  const { itens, valorTotal, limpar } = useContext(CarrinhoContext);
+
   return (
-    <Area titulo="Carrinho" cor="green">
-      {itens.length === 0 ? <CarrinhoVazio /> : <div>Tem itens...</div>}
+    <Area titulo="Carrinho" cor="green" sumario={Real.format(valorTotal)}>
+      <div className="flex flex-col items-center gap-5">
+        <div className="flex items-center flex-wrap justify-center gap-4">
+          {itens.length === 0 ? (
+            <CarrinhoVazio />
+          ) : (
+            itens.map((item) => (
+              <ItemCarrinhoCard key={item.produto.id} item={item} />
+            ))
+          )}
+        </div>
+        {itens.length > 0 && (
+          <button
+            className="flex gap-2 justify-center w-28 btn-danger"
+            onClick={limpar}
+          >
+            <IconX /> Limpar
+          </button>
+        )}
+      </div>
     </Area>
   );
 }
--- cursos/cod3r/formacao-dev/react/gerenciamento-estado/src/context/CarrinhoContext.tsx
+++ cursos/cod3r/formacao-dev/react/gerenciamento-estado/src/context/CarrinhoContext.tsx
@@ -4,7 +4,10 @@ import ItemCarrinho from "@/data/model/ItemCarrinho";
 
 interface CarrinhoContextProps {
   itens: ItemCarrinho[];
+  valorTotal: number;
   adicionarItem: (produto: Produto) => void;
+  removerItem: (produto: Produto) => void;
+  limpar: () => void;
 }
 
 const CarrinhoContext = createContext<CarrinhoContextProps>({} as any);
@@ -13,7 +16,40 @@ export function CarrinhoProvider(props: PropsWithChildren) {
   const [itens, setItens] = useState<ItemCarrinho[]>([]);
 
   function adicionarItem(produto: Produto) {
-    setItens([...itens, { produto, quantidade: 1 }]);
+    const item = itens.find((i) => i.produto.id === produto.id) ?? {
+      produto,
+      quantidade: 0,
+    };
+    const novoItem = { ...item, quantidade: item.quantidade + 1 };
+    const outrosItens = itens.filter((i) => i.produto.id !== produto.id);
+
+    setItens([...outrosItens, novoItem].sort(ordenarItem));
+  }
+
+  function removerItem(produto: Produto) {
+    const novosItens = itens
+      .map((item) => {
+        return item.produto.id === produto.id
+          ? { ...item, quantidade: item.quantidade - 1 }
+          : item;
+      })
+      .filter((item) => item.quantidade > 0);
+
+    setItens(novosItens);
+  }
+
+  function limpar() {
+    setItens([]);
+  }
+
+  function calcularValorTotal() {
+    return itens.reduce((total: number, item: ItemCarrinho) => {
+      return total + item.quantidade * item.produto.preco;
+    }, 0);
+  }
+
+  function ordenarItem(a: ItemCarrinho, b: ItemCarrinho) {
+    return a.produto.nome > b.produto.nome ? 1 : -1;
   }
 
   return (
@@ -21,6 +57,11 @@ export function CarrinhoProvider(props: PropsWithChildren) {
       value={{
         itens,
         adicionarItem,
+        removerItem,
+        limpar,
+        get valorTotal() {
+          return calcularValorTotal();
+        },
       }}
     >
       {props.children}
